337dc3c1f12ae424377a70e1f8bf630ea479c17f
AFauzulh <alfirsaafauzulh@gmail.com>
2020-11-21 13:30:40 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+});
