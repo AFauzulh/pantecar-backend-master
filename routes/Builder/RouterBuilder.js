ea3475df8fb4659c67f7f939cdaea82ca4e4b210
AFauzulh <alfirsaafauzulh@gmail.com>
2020-11-30 22:26:24 +0700

    add builder for router

--- routes/Builder/RouterBuilder.js
+++ routes/Builder/RouterBuilder.js
@@ -2,7 +2,7 @@ const indexRoutes = require('./indexRoutes');
 
 module.exports = class Routes {
     constructor(app) {
-        // Register the router here
+        // Register routers here
         app.use("/index", indexRoutes);
     }
 }
