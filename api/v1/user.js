c385ccaab40cfa566a8618f6f4729b2e6af6c5ce
AFauzulh <alfirsaafauzulh@gmail.com>
2020-12-04 22:47:23 +0700

    add get user by id and get all users API

--- api/v1/user.js
+++ api/v1/user.js
@@ -1,2 +1,48 @@
 const User = require('../../models/User');
 const RentalShop = require('../../models/RentalShop');
+
+
+exports.getUserById = async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        const user = await User.findByPk(id, {
+            attributes: { exclude: ['password'] }
+        });
+
+        if (!user) {
+            const error = new Error("user not found !");
+            error.statusCode = 404;
+            throw error;
+        }
+
+        res.status(200).json({
+            data: {
+                user: user
+            }
+        });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+exports.fetchAll = async (req, res, next) => {
+    try {
+        const user = await User.findAll();
+
+        res.status(200).json({
+            data: {
+                user: user
+            }
+        });
+
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,4 +3,8 @@ const userAPI = require('../api/v1/user');
 
 const router = express.Router();
 
+router.get('/users', userAPI.fetchAll);
+router.get('/:id', userAPI.getUserById);
+
+
 module.exports = router;
