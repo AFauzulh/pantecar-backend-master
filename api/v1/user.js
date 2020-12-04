dd44f91984246d7ca88bfe53a23811797a8b7ddc
AFauzulh <alfirsaafauzulh@gmail.com>
2020-12-05 03:11:40 +0700

    add check is user have a rental shop API

--- api/v1/user.js
+++ api/v1/user.js
@@ -29,6 +29,44 @@ exports.getUserById = async (req, res, next) => {
     }
 };
 
+exports.checkHaveRentalShop = async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        const foundedUser = await User.findByPk(id);
+
+        if (!foundedUser) {
+            const error = new Error("user not found !");
+            error.statusCode = 404;
+            throw error;
+        }
+
+        const rentalShopUser = await RentalShop.findOne({
+            where: { userIdUser: id }
+        });
+
+        if (!rentalShopUser) {
+            res.status(200).json({
+                data: {
+                    status: false
+                }
+            });
+        }
+
+        res.status(200).json({
+            data: {
+                status: true
+            }
+        });
+
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+}
+
 exports.fetchAll = async (req, res, next) => {
     try {
         const user = await User.findAll();
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,7 @@ const userAPI = require('../api/v1/user');
 const router = express.Router();
 
 router.get('/users', userAPI.fetchAll);
+router.get('/check-rentalshop/:id', userAPI.checkHaveRentalShop);
 router.get('/:id', userAPI.getUserById);
 
 
